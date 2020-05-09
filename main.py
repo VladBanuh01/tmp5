276b3d302c808e3da3c56390778e4f2da753f7d9
Vlad Banuh <you@example.com>
2020-05-09 18:04:26 +0300

    first

---
+++ main.py
@@ -0,0 +1,25 @@
+print('hello')
+a = {}
+a.update({'name': 'vlad'})
+print(a)
+c = '''arfar
+... argadgatg
+... ategaethryhyr'''
+c
+'arfar\nargadgatg\nategaethryhyr'
+print(c)
+s1 = 'sava'
+s2 = 'eggs'
+print(s1 + s2)
+print('spam' * 5)
+s = 'savaeggs'
+i = 6
+while i < 15:
+    print(i)
+    i = i + 2
+    pass
+for i in 'hello world':
+    if i == 'o':
+        continue
+    print(i * 2, end='')
+
